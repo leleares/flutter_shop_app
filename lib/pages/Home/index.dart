64cefc1b90684ab0a671b1dfdb5d57db3a91c099
wanglele <wanglele@luojilab.com>
2026-04-25 00:38:12 +0800

    Add pull down refresh page

--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -5,6 +5,7 @@ import 'package:flutter_shop_app/components/Home/Hot.dart';
 import 'package:flutter_shop_app/components/Home/MoreList.dart';
 import 'package:flutter_shop_app/components/Home/Recommend.dart';
 import 'package:flutter_shop_app/components/Home/Slider.dart';
+import 'package:flutter_shop_app/utils/ToastUtil.dart';
 import 'package:flutter_shop_app/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
@@ -21,6 +22,9 @@ class _HomeViewState extends State<HomeView> {
   final int _pageSize = 10;
   bool _isLoading = false;
   bool _hasMore = true;
+  // 创建一个 gloablkey，绑定到 widget 上，用于操纵 widget，类似于 react 中的 ref
+  final GlobalKey<RefreshIndicatorState> _key = GlobalKey();
+  double _paddingTop = 100;
 
   List<BannerItem> _bannerList = [];
   List<CategoryItem> _categoryList = [];
@@ -77,29 +81,39 @@ class _HomeViewState extends State<HomeView> {
     ];
   }
 
+  Future<void> _initData() async {
+    try {
+      await _getBannerList();
+      await _getCategoryList();
+      await _getHotPreferenceData();
+      await _getInVogueData();
+      await _getOneStopData();
+      await _getRecommendList();
+      setState(() {});
+      print("数据加载完毕");
+    } catch (e) {
+      print("初始化数据异常: $e");
+    }
+  }
+
   Future<void> _getBannerList() async {
     _bannerList = await getBannerList();
-    setState(() {});
   }
 
   Future<void> _getCategoryList() async {
     _categoryList = await getCategoryList();
-    setState(() {});
   }
 
   Future<void> _getHotPreferenceData() async {
     _hotPreferenceResult = await getHotPreferenceData();
-    setState(() {});
   }
 
   Future<void> _getInVogueData() async {
     _hotInVogueResult = await getHotInVogueData();
-    setState(() {});
   }
 
   Future<void> _getOneStopData() async {
     _hotOnStopResult = await getHotOnStopData();
-    setState(() {});
   }
 
   Future<void> _getRecommendList() async {
@@ -135,25 +149,46 @@ class _HomeViewState extends State<HomeView> {
     });
   }
 
+  Future<void> _onRefresh() async {
+    print("下拉刷新");
+    // 重置分页状态
+    _isLoading = false;
+    _page = 1;
+    _hasMore = true;
+    // 重新fetch数据
+    _initData();
+    Toastutil.showToast(context, "加载成功");
+    _paddingTop = 0;
+  }
+
   @override
   void initState() {
     super.initState();
     _registerEvent();
-    _getBannerList();
-    _getCategoryList();
-    _getHotPreferenceData();
-    _getInVogueData();
-    _getOneStopData();
-    _getRecommendList();
+    Future.microtask(() {
+      // 代码驱动方式触发页面下拉刷新
+      _key.currentState
+          ?.show(); // 可以理解为使用 ref 操作 dom，因此需确保 dom 渲染完毕，所以放到了微任务里，initState -> build -> microTask
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: CustomScrollView(
-        slivers: _getSliverList(),
-        controller: _scrollController,
+    return RefreshIndicator(
+      key: _key,
+      onRefresh: _onRefresh,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        padding: EdgeInsets.only(
+          top: _paddingTop,
+        ), // 模拟实现下拉刷新的距离（为了初始化页面时实现逼真的下拉刷新效果）
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: CustomScrollView(
+            slivers: _getSliverList(),
+            controller: _scrollController,
+          ),
+        ),
       ),
     );
   }
---
+++ lib/utils/ToastUtil.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Toastutil {
+  static void showToast(BuildContext context, String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        width: 120,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.all(Radius.circular(40)),
+        ),
+        behavior: SnackBarBehavior.floating,
+        content: Text(msg, textAlign: TextAlign.center),
+      ),
+    );
+  }
+}
