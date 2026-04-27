a5b441e00793cbe191e24529cc351a3380e68aeb
wanglele <wanglele@luojilab.com>
2026-04-27 11:27:31 +0800

    Add toke manager

--- lib/constants/index.dart
+++ lib/constants/index.dart
@@ -3,6 +3,7 @@ class GlobalConstants {
   static const String BASE_URL = "https://meikou-api.itheima.net"; // 基础域名
   static const int TIME_OUT = 10; // 超时时间
   static const String SUCCESS_CODE = "1"; // 成功状态码
+  static const String TOKEN_KEY = "shop_app_token";
 }
 
 // 请求接口地址api常量
@@ -14,5 +15,5 @@ class HttpConstants {
   static const String HOT_ONESTOP = "/hot/oneStop"; // 一站买全
   static const String HOT_RECOMMEND = "/home/recommend"; // 推荐列表
   static const String GUESS_YOU_LIKE = "/home/goods/guessLike"; // 猜你喜欢
-   static const String LOGIN = "/login"; // 登录请求
+  static const String LOGIN = "/login"; // 登录请求
 }
---
+++ lib/stores/TokenManager.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_shop_app/constants/index.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TokenManager {
+  Future<SharedPreferences> _getInstance() {
+    return SharedPreferences.getInstance();
+  }
+
+  String _token = "";
+
+  void init() async {
+    final prefs = await _getInstance();
+    _token = prefs.getString(GlobalConstants.TOKEN_KEY) ?? "";
+  }
+
+  void setToken(String val) async {
+    final prefs = await _getInstance();
+    prefs.setString(GlobalConstants.TOKEN_KEY, val);
+    // 更新 token
+    _token = val;
+  }
+
+  String getToken() {
+    return _token;
+  }
+
+  void removeToken() async {
+    final prefs = await _getInstance();
+    prefs.remove(GlobalConstants.TOKEN_KEY);
+    _token = "";
+  }
+}
+
+final tokenManager = TokenManager();
